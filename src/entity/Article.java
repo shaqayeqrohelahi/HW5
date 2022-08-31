5bde3236f2e11fef07873d0e5d0decf23c96710c
Shaqayeq Rohelahi <rohelahi.shaqayeq@gmail.com>
2022-08-31 23:52:55 +0430

    add main class and packaging

---
+++ src/entity/Article.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Article {
+    private int id;
+    private String title;
+    private String brief;
+    private String content;
+    private Date createDate;
+    private Boolean isPublished;
+    private User user;
+}
---
+++ src/entity/User.java
@@ -0,0 +1,47 @@
+public class User {
+    private int id;
+    private String username;
+    private String nationalCode;
+    private int birthday;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(String nationalCode) {
+        this.nationalCode = nationalCode;
+    }
+
+    public int getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(int birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
